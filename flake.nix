1c272543449b7deee893489a569dfb35fa2a84b8
Raghavendra Bhuvan <rage28@gmail.com>
2025-02-14 23:16:27 +0530

    Remove unused templates, update go to 1.24

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
-  description = "Ready-made templates for easily creating flake-driven environments";
+  description = "Nix based dev-env";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.*";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
@@ -118,44 +118,9 @@
           description = "C/C++ development environment";
         };
 
-        clojure = {
-          path = ./clojure;
-          description = "Clojure development environment";
-        };
-
-        csharp = {
-          path = ./csharp;
-          description = "C# development environment";
-        };
-
-        cue = {
-          path = ./cue;
-          description = "Cue development environment";
-        };
-
-        dhall = {
-          path = ./dhall;
-          description = "Dhall development environment";
-        };
-
-        elixir = {
-          path = ./elixir;
-          description = "Elixir development environment";
-        };
-
-        elm = {
-          path = ./elm;
-          description = "Elm development environment";
-        };
-
         empty = {
           path = ./empty;
           description = "Empty dev template that you can customize at will";
-        };
-
-        gleam = {
-          path = ./gleam;
-          description = "Gleam development environment";
         };
 
         go = {
@@ -163,54 +128,14 @@
           description = "Go (Golang) development environment";
         };
 
-        hashi = {
-          path = ./hashi;
-          description = "HashiCorp DevOps tools development environment";
-        };
-
-        haskell = {
-          path = ./haskell;
-          description = "Haskell development environment";
-        };
-
         java = {
           path = ./java;
           description = "Java development environment";
         };
 
-        jupyter = {
-          path = ./jupyter;
-          description = "Jupyter development environment";
-        };
-
-        kotlin = {
-          path = ./kotlin;
-          description = "Kotlin development environment";
-        };
-
-        latex = {
-          path = ./latex;
-          description = "LaTeX development environment";
-        };
-
-        lean4 = {
-          path = ./lean4;
-          description = "Lean 4 development environment";
-        };
-
         lua = {
           path = ./lua;
           description = "Lua development environment";
-        };
-
-        nickel = {
-          path = ./nickel;
-          description = "Nickel development environment";
-        };
-
-        nim = {
-          path = ./nim;
-          description = "Nim development environment";
         };
 
         nix = {
@@ -233,29 +158,9 @@
           description = "Open Policy Agent development environment";
         };
 
-        php = {
-          path = ./php;
-          description = "PHP development environment";
-        };
-
-        platformio = {
-          path = ./platformio;
-          description = "PlatformIO development environment";
-        };
-
         protobuf = {
           path = ./protobuf;
           description = "Protobuf development environment";
-        };
-
-        pulumi = {
-          path = ./pulumi;
-          description = "Pulumi development environment";
-        };
-
-        purescript = {
-          path = ./purescript;
-          description = "Purescript development environment";
         };
 
         python = {
@@ -263,49 +168,9 @@
           description = "Python development environment";
         };
 
-        r = {
-          path = ./r;
-          description = "R development environment";
-        };
-
-        ruby = {
-          path = ./ruby;
-          description = "Ruby development environment";
-        };
-
-        rust = {
-          path = ./rust;
-          description = "Rust development environment";
-        };
-
-        rust-toolchain = {
-          path = ./rust-toolchain;
-          description = "Rust development environment with Rust version defined by a rust-toolchain.toml file";
-        };
-
-        scala = {
-          path = ./scala;
-          description = "Scala development environment";
-        };
-
         shell = {
           path = ./shell;
           description = "Shell script development environment";
-        };
-
-        swi-prolog = {
-          path = ./swi-prolog;
-          description = "Swi-prolog development environment";
-        };
-
-        swift = {
-          path = ./swift;
-          description = "Swift development environment";
-        };
-
-        vlang = {
-          path = ./vlang;
-          description = "Vlang developent environment";
         };
 
         zig = {
@@ -316,7 +181,6 @@
         # Aliases
         c = c-cpp;
         cpp = c-cpp;
-        rt = rust-toolchain;
       };
     };
 }
